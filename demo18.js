3e2f0e689c9a145d345bdabc73a4ef92ed497380
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-12 17:57:26 +0500

    JSON object

---
+++ demo18.js
@@ -0,0 +1,25 @@
+const course = {
+    coursename: "sindh board ",
+    price:"999",
+    courseInstructor:"Masood" 
+}
+
+//course.courseInstructor
+
+const {courseInstructor:instructor}= course
+ //console.log(courseInstructor);
+ console.log(instructor);
+
+
+//  {
+//     "name": "Masoo",
+//     "coursename": "sindh board teaching", //JSON (javascript object notation)
+//     "price": "free"
+//  }
+
+
+[
+    {},
+    {},
+    {}
+]
